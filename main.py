887d534a2111e04cab231fbdaba320d2235e42b1
mardanovaAA <mardanova.aa@phystech.edu>
2020-12-10 18:43:57 +0300

    Clear code

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from tkinter import *
-from tkinter.filedialog import *
-from classes import *
+import tkinter as tk
+from tkinter import filedialog as fd
+import classes as cl
 import csv
 import numpy as np
 import matplotlib.pyplot as plt
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 layer_objects = []
 
 
-root = Tk()
+root = tk.Tk()
 root.geometry('500x500')
 root.resizable(width = True, height = True)
 
@@ -23,7 +23,7 @@ def input_file(input_filename):
     with open(input_filename) as File:
         reader = csv.reader(File, delimiter = ',')
         for row in reader:
-            sloy = Layer()
+            sloy = cl.Layer()
             parse_parameters(row, sloy)
             sloi.append(sloy)
     return sloi
@@ -32,7 +32,7 @@ def save_file_dialog():
     '''
     Function that opens the file saving window
     '''
-    out_filename = asksaveasfilename(filetypes=(("CSV Files", "*.csv"),))
+    out_filename = fd.asksaveasfilename(filetypes = (("CSV Files", "*.csv"),))
     output_write(out_filename, layer_objects)
 
 def output_write(output_filename, layer_objects):
@@ -43,9 +43,9 @@ def output_write(output_filename, layer_objects):
     '''
     data = []
     for obj in layer_objects:
-        line = [obj.number,obj.name,obj.permittivity,obj.thickness]
+        line = [obj.number, obj.name, obj.permittivity, obj.thickness]
         data.append(line)
-    with open(output_filename, 'w', newline='') as File:
+    with open(output_filename, 'w', newline = '') as File:
         writer = csv.writer(File)
         writer.writerows(data)
 
@@ -68,7 +68,7 @@ def open_file_dialog():
     global layer_objects
     #global perform_execution
     #perform_execution = False
-    in_filename = askopenfilename(filetypes=(("CSV Files", "*.csv"),))
+    in_filename = fd.askopenfilename(filetypes = (("CSV Files", "*.csv"),))
     layer_objects.extend(input_file(in_filename))
 
 
@@ -79,12 +79,12 @@ def Layer_list():
     for widget in flist.winfo_children():
         widget.destroy()
 
-    lbox = Listbox(flist,width = 50, height = 50)
+    lbox = tk.Listbox(flist, width = 50, height = 50)
 
     for obj in layer_objects:
         line = '#' + str(obj.number) + '  ' + 'Name: ' + str(obj.name) + '  ' + 'Permittivity: ' +str(obj.permittivity) + '  ' + 'Thickness: ' + str(obj.thickness)
-        lbox.insert(END, line)
-    lbox.pack(side=LEFT)
+        lbox.insert(tk.END, line)
+    lbox.pack(side = tk.LEFT)
 
 
 
@@ -100,57 +100,57 @@ def add_layer():
 
         '''
         number = 0
-        transport = Layer()
+        transport = cl.Layer()
 
-        newlayer = Layer()
+        newlayer = cl.Layer()
 
-        newlayer.number=int(tnumber.get())
+        newlayer.number = int(tnumber.get())
 
-        newlayer.name=str(tname.get())
+        newlayer.name = str(tname.get())
 
-        newlayer.permittivity=complex(tpermittivity.get())
+        newlayer.permittivity = complex(tpermittivity.get())
 
-        newlayer.thickness=float(tthickness.get())
+        newlayer.thickness = float(tthickness.get())
         layer_objects.append(newlayer)
-        for i in range(len(layer_objects)-1):
-            for j in range((len(layer_objects)-i-1)):
-                if layer_objects[j].number >= layer_objects[j+1].number:
+        for i in range(len(layer_objects) - 1):
+            for j in range((len(layer_objects) - i - 1)):
+                if layer_objects[j].number >= layer_objects[j + 1].number:
                     transport = layer_objects[j]
-                    layer_objects[j] = layer_objects[j+1]
-                    layer_objects[j+1] = transport
-        for i in range(int(tnumber.get()), len(layer_objects)-1 ):
+                    layer_objects[j] = layer_objects[j + 1]
+                    layer_objects[j + 1] = transport
+        for i in range(int(tnumber.get()), len(layer_objects) - 1):
             layer_objects[i].number += 1
         layer_objects[-1].number += 1
         addlayerwindow.destroy()
 
-    tnumber = StringVar()
-    tname = StringVar()
-    tthickness = StringVar()
-    tpermittivity = StringVar()
+    tnumber = tk.StringVar()
+    tname = tk.StringVar()
+    tthickness = tk.StringVar()
+    tpermittivity = tk.StringVar()
 
-    addlayerwindow = Toplevel(root)
+    addlayerwindow = tk.Toplevel(root)
 
-    lnumber=Label(addlayerwindow,text='Номер слоя')
+    lnumber = tk.Label(addlayerwindow, text = 'Номер слоя')
     lnumber.pack()
-    enumber = Entry(addlayerwindow,width=50, textvariable = tnumber )
+    enumber = tk.Entry(addlayerwindow, width = 50, textvariable = tnumber )
     enumber.pack()
 
-    lname=Label(addlayerwindow,text='Имя слоя')
+    lname = tk.Label(addlayerwindow, text = 'Имя слоя')
     lname.pack()
-    ename = Entry(addlayerwindow,width=50, textvariable = tname)
+    ename = tk.Entry(addlayerwindow, width = 50, textvariable = tname)
     ename.pack()
 
-    lpermittivity=Label(addlayerwindow,text='Проницаемость')
+    lpermittivity = tk.Label(addlayerwindow, text = 'Проницаемость')
     lpermittivity.pack()
-    epermittivity = Entry(addlayerwindow,width=50, textvariable = tpermittivity)
+    epermittivity = tk.Entry(addlayerwindow, width = 50, textvariable = tpermittivity)
     epermittivity.pack()
 
-    lthickness=Label(addlayerwindow,text='Толщина')
+    lthickness = tk.Label(addlayerwindow, text='Толщина')
     lthickness.pack()
-    ethickness = Entry(addlayerwindow,width=50, textvariable = tthickness)
+    ethickness = tk.Entry(addlayerwindow, width=50, textvariable = tthickness)
     ethickness.pack()
 
-    add = Button(addlayerwindow, text='Добавить слой',command=add_lay)
+    add = tk.Button(addlayerwindow, text='Добавить слой', command = add_lay)
     add.pack()
 
 def Builtgraph():
@@ -162,31 +162,30 @@ def Builtgraph():
     f = 0
     p = plt.subplot(111)
     for i in layer_objects:
-        f = f + i.thickness*i.permittivity.real
-    f = f/len(layer_objects)
-    plt.plot(angle, angle*f)
+        f = f + i.thickness * i.permittivity.real
+    f = f / len(layer_objects)
+    plt.plot(angle, angle * f)
     plt.show()
 
 #Buttons
-frame = Frame(root)
-frame.pack(side=BOTTOM)
-add_layer_button = Button(frame, text="Добавить слой", command=add_layer)
-add_layer_button.pack(side=LEFT)
-addgraph=Button(frame,text='Построить график',command=Builtgraph)
+frame = tk.Frame(root)
+frame.pack(side = tk.BOTTOM)
+add_layer_button = tk.Button(frame, text = "Добавить слой", command = add_layer)
+add_layer_button.pack(side = tk.LEFT)
+addgraph = tk.Button(frame, text = 'Построить график', command = Builtgraph)
 addgraph.pack()
-flist = Frame(root)
-flist.pack(side=LEFT)
+flist = tk.Frame(root)
+flist.pack(side = tk.LEFT)
 
 #Main menu on the top of the window
-mainmenu = Menu(root)
-root.config(menu=mainmenu)
-filemenu = Menu(mainmenu, tearoff=0)
-filemenu.add_command(label="Открыть...", command =open_file_dialog)
-filemenu.add_command(label="Сохранить файл как...", command = save_file_dialog)
-mainmenu.add_cascade(label="Файл",
-                     menu=filemenu)
-mainmenu.add_command(label="Показать список слоёв", command = Layer_list)
-mainmenu.add_command(label="Справка")
+mainmenu = tk.Menu(root)
+root.config(menu = mainmenu)
+filemenu = tk.Menu(mainmenu, tearoff = 0)
+filemenu.add_command(label = "Открыть...", command = open_file_dialog)
+filemenu.add_command(label = "Сохранить файл как...", command = save_file_dialog)
+mainmenu.add_cascade(label = "Файл", menu=filemenu)
+mainmenu.add_command(label = "Показать список слоёв", command = Layer_list)
+mainmenu.add_command(label = "Справка")
 
 
 
@@ -195,7 +194,7 @@ mainmenu.add_command(label="Справка")
 
 #testmenu=Mainmenu(root)
 
-mainloop()
+tk.mainloop()
 
 
 
