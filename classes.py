fd908f20b4ca78531bd8b2b92fa347b18a21697b
Kharichkin <kharichkin.is@phystech.edu>
2020-12-11 11:08:29 +0300

    Украсил classes, добавил файлы с данными

--- classes.py
+++ classes.py
@@ -1,142 +1,99 @@
 import tkinter as tk
 
+
 class Layer():
     """
-Layer of Material
+    Layer of a material.
+    """
+    def __init__(self, number, name, refractive_index, thickness):
+        """
 
-number - layer number
-thickness - layer thickness
-permittivity - dielectric constant
-"""
-
-    def __init__(self):
-        self.number = 0
-        self.thickness = 0
-        self.refractive_index = 0
-        self.name = ''
-
-    def get_number(self):
-        return self.number
-
-    def get_thickness(self):
-        return self.thickness
-
-    def get_permittivity(self):
-        return self.refractive_index
-
-    def set_number(self, number):
+        @param int number: number of the layer
+        @param str name: name of the layer
+        @param complex refractive_index: refractive index of the layer material
+        @param float thickness: thickness of the layer
+        """
         self.number = number
-
-    def set_thickness(self, thickness):
+        self.name = name
+        self.refractive_index = refractive_index
         self.thickness = thickness
-
-    def set_permittivity(self, permittivity):
-        self.permittivity = permittivity
-
 
 
 class LayerWidget():
     """
-Layer image on the screen
+    Layer image on the screen.
+    """
+    def __init__(self, canvas, layer, x, y, width=80, length=100):
+        """
 
+        @param tk.canvas canvas: the canvas for image of the layer
+        @param class Layer() layer: the layer
+        @param int x, int y: coordinates of the upper left corner of the layer
+        @param int width: width of the layer image (The default is 80 pixels)
+        @param int length: the length of the layer image (The default is 100 pixels)
+        @param tk.canvas.rectangle rectangle: the widget of the layer
+        @param name: str - layer name
+        """
+        self.canvas = canvas
+        self.layer = layer
+        self.x = x
+        self.y = y
+        self.width = width
+        self.length = length
+        self.rectangle = self.canvas.create_rectangle(x, y, x + length, y + width, fill='peach puff', width=2)
+
+    def set_text(self, name=None, comment=[]):
+        """
+        Creates text on layer image;
+        
+        Parameters
+        ----------
+        name: str - the name of layer (the default is None);
+        comment: - the comment for the layer (the default is []);
+        """
+        if name == None:
+            self.name = 'Layer №' + str(self.layer.number)
+        else:
+            self.name = name
+        self.comment = comment
+        self.text_layer = self.canvas.create_text(self.x + self.length // 2, self.y + self.width // 2,
+                                                  text=self.name + '\n' + self.comment + '\n', font='20')
+        self.canvas.itemconfig(self.text_layer)
+
+    def buttons(self):
+        """
+        creates a button.
+        """
+        self.button = tk.Button(self.canvas, text='')
+        self.button['text'] = 'Choose'
+        self.button['activebackground'] = 'bisque3'
+        self.button['activeforeground'] = 'bisque3'
+        self.button['bg'] = 'bisque2'
+        self.button.place(x=self.x - int(1.3 * self.length), y=self.y + int(0.25 * self.width),
+                          width=self.length, height=self.width // 2)
+
+    def move(event):
+        x = event.x
+        y = event.y
+        s = "Движение мышью {}x{}".format(x, y)
+        tk.root.title(s)
+
+
+class GraphWidget:
+    """
+Graphics on the screen
+x, y - coordinates of the upper left end of the image
+name - graphic name
+comment - comment on the image
 """
-    def __init__(self, screen, layers):
-        """
-        Parameters
-        ----------
-        screen: tk.root or tk.frame - the place, where will be Layers
-        layers: list of class Layer() - the layer;
-        """
-        
-        self.frame_pic = tk.Frame(screen)
-        self.canv = tk.Canvas(self.frame_pic)
-        self.menu_layers = tk.Frame(screen)
-        self.layers = layers
-        
-    #def delete_lay(self, lay):
-        
-    """    
-    def creat_menu(self, lay):
-        tname = tk.StringVar()
-        tthickness = tk.StringVar()
-        tpermittivity = tk.StringVar()
-        
-        lnubmber = tk.Label(self.menu_layers, text = 'Поменяйте слой №' + str(lay.number))
-        lnubmber.pack()
-        
-        lname = tk.Label(self.menu_layers, text = 'Имя слоя')
-        lname.pack()
-        ename = tk.Entry(self.menu_layers, width = 25, textvariable = tname)
-        ename.pack()
-        
-        lpermittivity = tk.Label(self.menu_layers, text = 'Проницаемость')
-        lpermittivity.pack()
-        epermittivity = tk.Entry(self.menu_layers, width = 25, textvariable = tpermittivity)
-        epermittivity.pack()
-        
-        lthickness = tk.Label(self.menu_layers, text='Толщина')
-        lthickness.pack()
-        ethickness = tk.Entry(self.menu_layers, width = 25, textvariable = tthickness)
-        ethickness.pack()
-        
-        Будущие кнопочки, сейчас доработаю
-        add = tk.Button(self.menu_layers, text='Внести изменения', command = self.change_lay(lay, tname, tthickness, tpermittivity))
-        add.pack()
-        
-        up = tk.Button(self.menu_layers, text='Поднять слой', command = self.move_up(lay))
-        up.pack()
-        
-        down = tk.Button(self.menu_layers, text='Опустить слой', command = self.move_down(lay))
-        down.pack()
-        
-        dele = tk.Button(self.menu_layers, text='Удалить слой', command = self.delete_lay(lay))
-        dele.pack()
-          """  
-        
-    def create_button(self, lay, fr):
-        """
-        Creates button of Layer
-        Parameters
-        ----------
-        lay : class Layer() - the certain layer
-        fr: frame, where wil be layers
-        
-        Return button of layer
-        """
-        text_but = '#' + str(lay.number) + '. ' + lay.name + '\n'
-        text_but += 'refractive_index: ' + str(lay.refractive_index) + '\n'
-        text_but += 'Thickness: ' + str(lay.thickness)
-        but = tk.Button(fr, text = text_but, width = 30, height = 4)
-        but['activebackground'] = 'CadetBlue2'
-        but['activeforeground'] = 'white'
-        but['bg'] = 'CadetBlue1'
-        return but
-        
-    def list_layers(self):
-        """
-        Creates picture of layers on the screen
-        """
-        scroll_y = tk.Scrollbar(self.frame_pic, orient = tk.VERTICAL, command = self.canv.yview)
-        scrollable_frame = tk.Frame(self.canv)
-        scrollable_frame.bind("<Configure>", lambda e: self.canv.configure(scrollregion = self.canv.bbox("all")))
-        self.canv.create_window((0, 0), window = scrollable_frame, anchor = "nw")
-        self.canv.configure(yscrollcommand = scroll_y.set)
-        
-        
-        for obj in self.layers:
-            button = self.create_button(obj, scrollable_frame)
-            button.pack(side = tk.TOP)
-            
-        self.frame_pic.pack(side = tk.RIGHT, fill = tk.Y)
-        scroll_y.pack(side = tk.LEFT, fill = tk.Y)  
-        self.canv.pack(side = tk.RIGHT, fill = tk.Y)
-         
-        
-    
-    def result(self):
-        self.menu_layers.pack(side = tk.LEFT, expand=True)
-        self.list_layers()
 
+    x = 0
 
+    y = 0
 
+    name = ''
 
+    comment = ''
+
+    """def set_text(name, comment):
+        canvas.create_text =()"""
